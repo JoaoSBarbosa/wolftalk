4bd1fe351e1a73542a5e69ee8f0680d40129f349
João Barbosa <contato.jsbarbosa@gmail.com>
2023-10-07 21:04:05 -0300

    Estilizado o avatar do sidebar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import "./App.css";
 export default () => {
   return (
     <div className="app-window">
-      <aside className="sidebar ">Área do sidebar</aside>
+      <aside className="sidebar ">
+        <header>
+          <div>
+            <img
+              className="header--avatar"
+              src="https://uploaddeimagens.com.br/images/004/631/029/full/avatar.jpg?1696723039"
+              alt=""
+            />
+          </div>
+          <div className="header-buttons">btn</div>
+        </header>
+        <div className="search">...</div>
+        <div className="chatList">...</div>
+      </aside>
       <section className="content-area">Área de conteúdo</section>
     </div>
   );
